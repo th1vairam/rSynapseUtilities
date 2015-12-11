fd3427f02e07270cee31a8bfa0196d367ce95180
kdaily <kdaily@users.noreply.github.com>
2015-12-11 15:43:57 -0800

    update copy file

--- R/copyFile.R
+++ R/copyFile.R
@@ -1,5 +1,29 @@
-copyFile <- function(fileId, parentId){
-  myFile <- synapseClient::synGet(fileId,downloadFile = FALSE);
-  myFile$properties$parentId <- parentId;
-  myFile <- synapseClient::synStore(myFile,forceVersion=F);
+copyFile <- function(fileId, parentId, version, setAnnotations=TRUE, setActivity=TRUE) {
+  # Get the file
+  myFile <- synapseClient::synGet(fileId, version=version,
+                                  downloadFile = TRUE)
+
+  # Create a new file
+  newFile <- File(file=synapseClient::getFileLocation(myFile),
+                  name=synapseClient::synGetProperties(myFile)$name,
+                  parentId=parentId)
+
+  if (setAnnotations) {
+  synSetAnnotations(newFile) <- synGetAnnotations(myFile)
+  }
+
+  if (setActivity) {
+    generatedBy(newFile) <- synGetActivity(myFile, version)
+  }
+
+  synapseClient::synStore(newFile)
+}
+
+copyFileAllVersions <- function(fileId, parentId) {
+  res <- synRestGET(sprintf('/entity/%s/version', fileId))
+
+  obj <- lapply(rev(res$results),
+                function(x) copyFile(fileId=x$id,
+                                     version=x$versionNumber))
+
 }

d15c3fc079e6360c96c414d4567855e9839c83c2
kdaily <kdaily@users.noreply.github.com>
2016-01-04 15:35:19 -0800

    change store, add logging.

--- R/copyFile.R
+++ R/copyFile.R
@@ -1,12 +1,18 @@
 copyFile <- function(fileId, parentId, version, setAnnotations=TRUE, setActivity=TRUE) {
   # Get the file
+  print(sprintf("Getting file %s", fileId))
   myFile <- synapseClient::synGet(fileId, version=version,
                                   downloadFile = TRUE)
+
+  print(sprintf("Got file to %s", synapseClient::getFileLocation(myFile)))
+  print(sprintf("Putting new file in %s", parentId))
 
   # Create a new file
   newFile <- synapseClient::File(path=synapseClient::getFileLocation(myFile),
                                  name=synapseClient::synGetProperties(myFile)$name,
                                  parentId=parentId)
+
+  print(sprintf("Created new file %s", synapseClient::synGetProperties(newFile)$name))
 
   if (setAnnotations) {
     synapseClient::synSetAnnotations(newFile) <- synapseClient::synGetAnnotations(myFile)
@@ -16,7 +22,8 @@ copyFile <- function(fileId, parentId, version, setAnnotations=TRUE, setActivity
     synapseClient::generatedBy(newFile) <- synapseClient::synGetActivity(myFile)
   }
 
-  synapseClient::synStore(newFile)
+  print(newFile)
+  newFile <- synapseClient::synStore(newFile)
 }
 
 copyFileAllVersions <- function(fileId, parentId) {

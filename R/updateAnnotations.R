8e3f38e7da7a3c5f9df8a21ce21318e4d00cc91d
Benjamin Logsdon <aeberhart@gmail.com>
2016-01-14 16:02:19 -0800

    audit single file, mint a DOI, and update the annotations

--- R/updateAnnotations.R
+++ R/updateAnnotations.R
@@ -1,9 +1,12 @@
-updateAnnotations <- function(synId,field,value){
+updateAnnotations <- function(synId,field,value,rmVar=NULL){
   library(synapseClient)
   synapseLogin()
   foo <- synGet(synId,downloadFile = F)
   bar <- synGetAnnotations(foo)
   bar <- as.list(bar)
+  if(!is.null(rmVar)){
+    bar <- bar[which(!names(bar)%in%rmVar)]
+  }
   bar[field] <- value
   synSetAnnotations(foo) <- bar
   foo <- synStore(foo,forceVersion=F)

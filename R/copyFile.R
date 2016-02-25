261180c7ccc989247f852a07bf48a4493d7fb545
kdaily <kdaily@users.noreply.github.com>
2016-02-25 11:00:29 -0800

    fix copying a local only file

--- R/copyFile.R
+++ R/copyFile.R
@@ -1,16 +1,31 @@
 copyFile <- function(fileId, parentId, version, setAnnotations=TRUE, setActivity=TRUE) {
   # Get the file
   print(sprintf("Getting file %s", fileId))
+
   myFile <- synapseClient::synGet(fileId, version=version,
-                                  downloadFile = TRUE)
+                                  downloadFile = FALSE)
+
+
+  if (!(grepl("^file:///",getFileLocation(myFile)))) {
+    myFile <- synapseClient::synGet(fileId, version=version,
+                                    downloadFile = TRUE)
+    # Create a new file
+    newFile <- synapseClient::File(path=synapseClient::getFileLocation(myFile),
+                                   name=synapseClient::synGetProperties(myFile)$name,
+                                   parentId=parentId)
+
+  }
+  else {
+    # Create a new file
+    newFile <- synapseClient::File(path=synapseClient::getFileLocation(myFile),
+                                   name=synapseClient::synGetProperties(myFile)$name,
+                                   parentId=parentId,
+                                   synapseStore=FALSE)
+  }
 
   print(sprintf("Got file to %s", synapseClient::getFileLocation(myFile)))
   print(sprintf("Putting new file in %s", parentId))
 
-  # Create a new file
-  newFile <- synapseClient::File(path=synapseClient::getFileLocation(myFile),
-                                 name=synapseClient::synGetProperties(myFile)$name,
-                                 parentId=parentId)
 
   print(sprintf("Created new file %s", synapseClient::synGetProperties(newFile)$name))
 
